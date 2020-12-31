c66d08fa02a7f19667d9bdd61aff21b70d39edfa
navana <onlywin7788@gmail.com>
2020-12-31 13:01:49 +0900

    commit

--- src/main/java/com/teros/dashboard/controller/page/data_dev/FlowDevPageController.java
+++ src/main/java/com/teros/dashboard/controller/page/data_dev/FlowDevPageController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FlowDevPageController {
 
-    @GetMapping("/flow-development/flow-design")
+    @GetMapping("/data-development/flow-dev")
     public String flowDesign(Model model) {
-        return "contents/data_dev/flow_design/index";
+        return "contents/data_dev/flow_dev/index";
     }
 
-    @GetMapping("/flow-development/connector")
+    @GetMapping("/data-development/connector")
     public String flowConnector(Model model) {
         return "contents/data_dev/connector/index";
     }
 
-    @GetMapping("/flow-development/data-set")
+    @GetMapping("/data-development/data-set")
     public String flowDataSet(Model model) {
         return "contents/data_dev/data_set/index";
     }

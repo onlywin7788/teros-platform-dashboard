c05715362c845fc98e89a734921785cd0114aeb8
navana <onlywin7788@gmail.com>
2021-12-05 10:52:12 +0900

    commit

--- src/main/java/com/teros/dashboard/controller/page/apiservice/APIServicePageController.java
+++ src/main/java/com/teros/dashboard/controller/page/apiservice/APIServicePageController.java
@@ -8,46 +8,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class APIServicePageController {
 
-    @GetMapping("/APIService/development/project")
+    // API Development
+    @GetMapping("/api-service/development/project")
     public String APIServiceProjectIndex(Model model) {
-        return "contents/APIService/development/project/index";
+        return "contents/api-service/development/project/index";
     }
 
-    @GetMapping("/APIService/development/api")
+    @GetMapping("/api-service/development/api")
     public String APIServiceAPIIndex(Model model) {
-        return "contents/APIService/development/api/index";
+        return "contents/api-service/development/api/index";
     }
 
-    @GetMapping("/APIService/development/api/create")
+    @GetMapping("/api-service/development/api/create")
     public String APIServiceAPICreateIndex(Model model) {
-        return "contents/APIService/development/api/create/index";
+        return "contents/api-service/development/api/create/index";
     }
 
-    @GetMapping("/APIService/development/api/create/module/method")
-    public String APIServiceAPICreateModuleMethod(Model model) {
-        return "contents/APIService/development/api/create/module/method";
+    @GetMapping("/api-service/group")
+    public String APIServiceAPIGroup(Model model) {
+        return "contents/api-service/group/index";
     }
 
-
-    @GetMapping("/api-development/api/contents/{id}")
-    public String developmentUpdate(Model model, @PathVariable long id) {
-        model.addAttribute("id", id);
-        model.addAttribute("update", 1);
-
-        return "contents/api_dev/api/contents/index";
-    }
-
-
-
-    // PAGE : API_PLAN
-    @GetMapping("/api-development/api-policy")
-    public String apiPlan(Model model) {
-        return "contents/api_dev/api_policy/index";
-    }
-
-    // PAGE : API_DEPLOY
-    @GetMapping("/api-development/api-deploy")
-    public String apiDeploy(Model model) {
-        return "contents/api_dev/api_deploy/index";
+    @GetMapping("/api-service/policy")
+    public String APIServiceAPIPolicy(Model model) {
+        return "contents/api-service/policy/index";
     }
 }
